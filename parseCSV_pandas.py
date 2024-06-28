7cf4618b8df511e4cbbad47a132144da0186db83
Zakebo <raphael.gouedic@gmail.com>
2024-06-28 03:42:28 +0200

    Feature Class and Feature Code deleted, rows with population < 1000 and population with not a numerical values deleted

--- parseCSV_pandas.py
+++ parseCSV_pandas.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 # Define the headers to be removed
-headers_to_remove = ["Geoname ID", "Admin1 Code", "Admin2 Code", "Admin3 Code", "Admin4 Code", "Modification date"]
+headers_to_remove = ["Geoname ID", "Admin1 Code", "Admin2 Code", "Admin3 Code", "Admin4 Code", "Modification date", "Feature Class","Feature Code"]
 
 # Input and output file names
 input_file = 'geonames-all-cities-with-a-population-1000.csv'
-output_file = 'geonames-all-cities-with-a-population-1000_OUTPUT2.csv'
+output_file = 'geonames-all-cities-with-a-population-1000_prod2.csv'
 
 def process_csv(input_file, output_file):
     # Read the CSV file
@@ -14,8 +14,9 @@ def process_csv(input_file, output_file):
     # Remove the specified headers
     df.drop(columns=headers_to_remove, inplace=True)
     
-    # Remove rows where Population is 0 or empty
-    df = df[df['Population'].notna() & (df['Population'] != 0)]
+    # Remove rows where Population is not a number, 0, empty, or below 1000
+    df['Population'] = pd.to_numeric(df['Population'], errors='coerce')
+    df = df[df['Population'].notna() & (df['Population'] >= 1000)]
 
     # Write the result to a new CSV file
     df.to_csv(output_file, sep=';', index=False)
